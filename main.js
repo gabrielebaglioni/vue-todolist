5662c660d1596ec96fc24bceb2331a7fb6383077
gabrielebaglioni <gabrielebaglioni55@gmail.com>
2022-05-23 15:46:44 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,25 @@
+const app = new Vue({
+    el: "app",
+    data: {
+        toDo: [{
+                text: "fare la spesa",
+                isDone: false
+            },
+            {
+                text: "fare uscire il cane",
+                isDone: false
+            },
+            {
+                text: "magiare ",
+                isDone: false
+            },
+            {
+                text: "fare il pieno",
+                isDone: false
+            },
+        ]
+    },
+    methods: {
+
+    },
+});
